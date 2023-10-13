d49e0ca5566fea2999746b70b12d3cfc9264b572
Gustavo Porto <gus@portothr.ee>
2023-10-13 19:14:34 +0000

    feat: use anyhow

--- rust-cli/grrs/src/main.rs
+++ rust-cli/grrs/src/main.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 use clap::Parser;
+use anyhow::{Context, Result};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -12,9 +13,10 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let file = File::open(&args.path)?;
+    let file = File::open(&args.path)
+        .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
     let mut reader = BufReader::new(file);
     let mut line = String::new();
 
