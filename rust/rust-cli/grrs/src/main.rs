d99aa21974df36a4abf6ab26e84bce376d1a6467
Gustavo Porto <gus@portothr.ee>
2024-04-17 16:48:06 -0300

    feat: add log and env_logger

--- rust/rust-cli/grrs/src/main.rs
+++ rust/rust-cli/grrs/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use log::{info, trace};
 use spinners::{Spinner, Spinners};
 use std::fs::File;
 use std::io::prelude::*;
@@ -16,6 +17,10 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
+
+    trace!("Starting up...");
+
     let mut sp = Spinner::new(Spinners::Dots9, "Loading...\n".into());
     sleep(Duration::from_secs(1));
     sp.stop();
@@ -23,6 +28,9 @@ fn main() -> Result<()> {
     let args = Cli::parse();
     let file = File::open(&args.path)
         .with_context(|| format!("Could not read file `{}`", args.path.display()))?;
+
+    info!("Reading file...");
+
     let mut reader = BufReader::new(file);
 
     let mut line = String::new();
@@ -30,6 +38,7 @@ fn main() -> Result<()> {
     while reader.read_line(&mut line)? > 0 {
         if line.contains(&args.pattern) {
             println!("{}", line);
+            info!("Pattern found in line: {}", line);
         }
         line.clear();
     }
