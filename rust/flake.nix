91f9fb3def254ab23f045f369b4959eb5f133983
Gustavo Porto <gus@portothr.ee>
2024-04-17 10:37:52 -0300

    chore: rust files

---
+++ rust/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "rust";
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    pre-commit-hooks = { url = "github:cachix/pre-commit-hooks.nix"; };
+  };
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        src = ./.;
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            inherit src;
+            hooks = {
+              nixfmt.enable = true;
+              shfmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+        };
+        devShell = import ./shell.nix {
+          inherit pkgs;
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+      });
+}
