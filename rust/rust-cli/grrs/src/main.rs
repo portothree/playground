d85d1b289e929d1d0779a349d439b8797e16c20f
Gustavo Porto <gus@portothr.ee>
2024-04-17 13:52:32 -0300

    feat: use BufReader

--- rust/rust-cli/grrs/src/main.rs
+++ rust/rust-cli/grrs/src/main.rs
@@ -1,13 +1,31 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 #[derive(Parser)]
+#[command(version, about, long_about = None)]
 struct Cli {
+    #[arg(long)]
     pattern: String,
+    #[arg(long)]
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Cli::parse();
+    let file = File::open(&args.path)?;
+    let mut reader = BufReader::new(file);
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    let mut content = String::new();
+
+    reader.read_to_string(&mut content)?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
