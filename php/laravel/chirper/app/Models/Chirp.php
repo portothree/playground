96db0976c03b92ced6a6e443129f1e09590d8b5f
Gustavo Porto <gustavo.porto@scratchpay.com>
2024-06-25 13:15:38 +0100

    feat: create and display chirps

--- php/laravel/chirper/app/Models/Chirp.php
+++ php/laravel/chirper/app/Models/Chirp.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
